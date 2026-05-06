3c9747c34e5c6507e3012674a89bb82c219835d2
Fladfisken <frederik.kvolbaek.pedersen@gmail.com>
2026-05-06 09:51:47 +0200

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHH V3

--- configuration.js
+++ configuration.js
@@ -20,22 +20,22 @@ export let obama;
 
 // Indlæser alle billeder. Kaldes i preload, så de er klar inden setup
 export function preloadAssets() {
-  cueBall = loadImage('0CueBall.png');
-  yellowFull = loadImage('1YellowFull.png');
-  blueFull = loadImage('2BlueFull.png');
-  redFull = loadImage('3RedFull.png');
-  purpleFull = loadImage('4PurpleFull.png');
-  orangeFull = loadImage('5OrangeFull.png');
-  greenFull = loadImage('6GreenFull.png');
-  maroonFull = loadImage('7MaroonFull.png');
-  eightBall = loadImage('8Ball.png');
-  yellowStripe = loadImage('9YellowStripe.png');
-  blueStripe = loadImage('10BlueStripe.png');
-  redStripe = loadImage('11RedStripe.png');
-  purpleStripe = loadImage('12PurpleStripe.png');
-  orangeStripe = loadImage('13OrangeStripe.png');
-  greenStripe = loadImage('14GreenStripe.png');
-  maroonStripe = loadImage('15MaroonStripe.png');
-  poolTable = loadImage('poolTable.png')
-  obama = loadImage('Obama.png')
+  cueBall = loadImage('./0CueBall.png');
+  yellowFull = loadImage('./1YellowFull.png');
+  blueFull = loadImage('./2BlueFull.png');
+  redFull = loadImage('./3RedFull.png');
+  purpleFull = loadImage('./4PurpleFull.png');
+  orangeFull = loadImage('./5OrangeFull.png');
+  greenFull = loadImage('./6GreenFull.png');
+  maroonFull = loadImage('./7MaroonFull.png');
+  eightBall = loadImage('./8Ball.png');
+  yellowStripe = loadImage('./9YellowStripe.png');
+  blueStripe = loadImage('./10BlueStripe.png');
+  redStripe = loadImage('./11RedStripe.png');
+  purpleStripe = loadImage('./12PurpleStripe.png');
+  orangeStripe = loadImage('./13OrangeStripe.png');
+  greenStripe = loadImage('./14GreenStripe.png');
+  maroonStripe = loadImage('./15MaroonStripe.png');
+  poolTable = loadImage('./poolTable.png')
+  obama = loadImage('./Obama.png')
 }
